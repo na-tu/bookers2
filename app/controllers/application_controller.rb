72cfc48f92860581118307408efb8d92c044cd41
na-tu <161991245+na-tu@users.noreply.github.com>
2024-05-28 13:56:18 +0000

    2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #もしdeviseの処理を行う場合、configure_permitted_parametersというものを実行して。
+  private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+    end
 end
